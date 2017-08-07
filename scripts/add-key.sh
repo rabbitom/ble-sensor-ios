612d285cfd991384bb92c556eb041b7efe707c28
erabbit <rabbitom@gmail.com>
2017-08-07 14:41:21 +0800

    modify add key script

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 security create-keychain -p travis ios-build.keychain
-security import ./scripts/ios-dist.p12 ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
+security import ./scripts/ios-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
