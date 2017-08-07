4ba97ea220fe76c62ea9b641b8bb31e7a28b4d51
erabbit <rabbitom@gmail.com>
2017-08-07 19:39:44 +0800

    profiles

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -2,10 +2,10 @@
 security create-keychain -p travis ios-build.keychain
 security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
-security import ./scripts/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./scripts/ios-dev.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
-security import ./scripts/ios-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
+security import profiles/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import profiles/ios-dev.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
+security import profiles/ios-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./scripts/$DEV_PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./scripts/$DIST_PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
+cp profiles/$DEV_PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
+cp profiles/$DIST_PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
