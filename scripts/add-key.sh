b33eea09eac1c96a1526f8dbb67d0b064071a3aa
erabbit <rabbitom@gmail.com>
2017-08-07 14:53:01 +0800

    set default keychain

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 security create-keychain -p travis ios-build.keychain
+security default-keychain -s ios-build.keychain
 security import ./scripts/ios-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
