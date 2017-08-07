c26d328155e9c1887c8d6e702016d0285cd4a752
erabbit <rabbitom@gmail.com>
2017-08-07 15:52:31 +0800

    add security command after import keys

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -4,5 +4,6 @@ security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security import ./scripts/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/ios-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
+security set-key-partition-list -S apple-tool:,apple: -s -k $P12_PASSWORD ios-build.keychain
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles
