6e7a510a2a8b5232e5af7315fe3666a2d2f6d886
erabbit <rabbitom@gmail.com>
2017-08-07 16:19:10 +0800

    add dev signing

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -3,6 +3,7 @@ security create-keychain -p travis ios-build.keychain
 security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security import ./scripts/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./scripts/ios-dev.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
 security import ./scripts/ios-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
