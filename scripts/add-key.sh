464836af4dbf5faf60f9bc6cbfe045c5e9521fb2
erabbit <rabbitom@gmail.com>
2017-08-07 22:07:25 +0800

    remove unused files

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -2,7 +2,6 @@
 security create-keychain -p travis ios-build.keychain
 security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
-security import profiles/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import profiles/ios-dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $P12_PASSWORD -T /usr/bin/codesign
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
